7e140cd0d866aab633d93159a6a4c78177e3e442
liu hang <liu hang>
2018-10-10 12:00:05 +0800

    init project the first 2 program of leetcode
    Signed-off-by: liu hang <liu hang>

---
+++ src/leetcode/AddTwoNumbers/AddTwoNumbers.java
@@ -0,0 +1,100 @@
+package leetcode.AddTwoNumbers;
+
+public class AddTwoNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ListNode a1 = new ListNode(2);
+		ListNode a2 = new ListNode(4);
+		ListNode a3 = new ListNode(3);
+		ListNode b1 = new ListNode(5);
+		ListNode b2 = new ListNode(6);
+		ListNode b3 = new ListNode(4);
+		a1.next=a2;
+		a2.next=a3;
+		b1.next=b2;
+		b2.next=b3;
+		ListNode resu = addTwoNumbers2(a1,b1);
+		while(resu.next!=null) {
+			System.out.println(resu.val);
+			resu = resu.next;
+		}
+		System.out.println("final");
+		System.out.println(resu.val);
+		System.out.println(15/10);
+	}
+
+	
+    public static ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+    	ListNode out = new ListNode(0);
+        ListNode outt = new ListNode(0);
+        boolean first = true;
+        int outval = 0;
+        int low_increase = 0;
+        do{
+        	int vout = l1.val+l2.val+low_increase;
+            if (vout>9){
+                outval = vout-10;
+                low_increase=1;
+            }else{
+                outval = vout;
+                low_increase=0;
+            }
+            if (first){
+                out.val = outval;
+                out.next = outt;
+                first=false;
+            }else{
+                outt.val = outval;
+                outt.next = new ListNode(0);
+                outt = outt.next;
+            }
+            l1=l1.next;
+            l2=l2.next;
+        }while(l1!=null);
+        
+        return out;
+    }
+    
+    
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode out = new ListNode(0);
+        ListNode outt = new ListNode(0);
+        boolean first = true;
+        int outval = 0;
+        int low_increase = 0;
+        while(l1!=null | l2!=null){
+        	int vout = 0;
+        	if(l1!=null && l2!=null) {
+        		vout = l1.val+l2.val+low_increase;
+        		l1=l1.next;
+                l2=l2.next;
+        	}else if (l2!=null) {
+        		vout = l2.val+low_increase;
+                l2=l2.next;
+        	}else {
+        		vout = l1.val+low_increase;
+        		l1=l1.next;
+        	}
+            if (vout>9){
+                outval = vout-10;
+                low_increase=1;
+            }else{
+                outval = vout;
+                low_increase=0;
+            }
+            if (first){
+            	outt = new ListNode(outval);
+                out = outt;
+                first = false;
+            }else{
+                outt.next = new ListNode(outval);
+                outt = outt.next;
+            }
+        }
+        if (low_increase!=0) {
+        	outt.next = new ListNode(low_increase);
+        }
+        return out;
+    }
+}
---
+++ src/leetcode/TwoSum/TwoSum.java
@@ -0,0 +1,34 @@
+package leetcode.TwoSum;
+
+public class TwoSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = {0,1,2} ;
+		System.out.println(a.length);
+		for(int i :a) {
+			System.out.println(i);
+		}
+		
+		System.out.println(a[1]+a[2]==3);
+		int si=1;
+		int sii = 2;
+		int[] q2 = {si,sii};
+		System.out.println(q2);
+		boolean ards = true;
+	}
+
+	
+	
+//    public int[] twoSum(int[] nums, int target) {
+//        int l = nums.length;
+//        for(int si=0;si<l-1;si++){
+//            for(int sii=si+1;sii<l;sii++){
+//                if(nums[si]+nums[sii]==target){
+//                    return {si,sii};
+//                }
+//            }
+//        }
+//        return null;
+//    }
+}
